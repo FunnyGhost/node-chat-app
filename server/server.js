833fd437ee55b69a96988ab5f5bd9c825c70f58e
Catalin Ciubotaru <catalinciubot@gmail.com>
2019-01-27 23:40:41 +0100

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -15,6 +15,16 @@ app.use(express.static(publicPath));
 io.on('connection', socket => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Catalin',
+    text: "What's up?",
+    createdAt: 123
+  });
+
+  socket.on('createMessage', message => {
+    console.log('New message', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('User disconnected');
   });
