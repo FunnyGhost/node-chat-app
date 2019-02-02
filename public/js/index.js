27caf1b6b389cc43f19bba7257d544caece12332
Catalin Ciubotaru <catalinciubot@gmail.com>
2019-02-02 21:43:32 +0100

    Added createMessage emit and listen

--- public/js/index.js
+++ public/js/index.js
@@ -11,3 +11,7 @@ socket.on('disconnect', function() {
 socket.on('newMessage', function(message) {
   console.log('New message', message);
 });
+
+socket.emit('createMessage', { from: 'Andra', text: 'Hi!' }, function(data) {
+  console.log('Got it!', data);
+});
